ca43108ab1d8f9c025025dbecc900626e552b058
Saninn Salas Diaz <5490201+distante@users.noreply.github.com>
2024-02-16 10:37:28 +0100

    refactor: give names to tests

--- html/assets/scripts.mjs
+++ html/assets/scripts.mjs
@@ -7,39 +7,4 @@ defineCustomElements(window).then(() => {
   document.querySelector('ion-button').addEventListener('click', (event) => {
     event.target.innerText = convertToTestString(event.target.innerText);
   });
-
-  document.querySelector('.ion-range-dual ion-range').value = {
-    lower: -50,
-    upper: 50,
-  };
-
-  // ------ ION RANGE ----- //
-  const ranges = document.querySelectorAll('ion-range');
-  const rangesIntervals = new Array(ranges.length);
-  document.querySelectorAll('ion-range').forEach((ionRange, i) => {
-    ionRange.addEventListener('ionChange', (event) => {
-      console.log('ionRange ionChange event', event);
-      clearInterval(rangesIntervals[i]);
-      const targetIonRange = event.target;
-
-      rangesIntervals[i] = setInterval(() => {
-        const slot =
-          targetIonRange.shadowRoot.querySelector('slot[name="end"]');
-        if (!slot) {
-          return;
-        }
-
-        clearInterval(rangesIntervals[i]);
-        rangesIntervals[i] = undefined;
-
-        let eventValue = event.detail.value;
-
-        if (typeof eventValue === 'object') {
-          eventValue = `${eventValue.lower}/${eventValue.upper}`;
-        }
-
-        slot.assignedNodes()[0].innerText = eventValue;
-      }, 100);
-    });
-  });
 });
