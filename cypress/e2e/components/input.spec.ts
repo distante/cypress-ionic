dc9514a706cdb18a15cdbb5061cc687b6a7eb3fc
Saninn Salas Diaz <5490201+distante@users.noreply.github.com>
2024-02-16 10:33:57 +0100

    feat(ion-input): add extra check for hydrated for ion-input

--- cypress/e2e/components/input.spec.ts
+++ cypress/e2e/components/input.spec.ts
@@ -2,42 +2,54 @@ import { ionInputCypress } from '@lib';
 import * as testHelpers from '../../../test-helpers/test-helpers.js';
 
 describe('Ion Input', () => {
-  const selector = 'ion-input';
-  beforeEach(() => {
-    cy.visit('./ion-input.html');
-  });
+  const selectors = [
+    '.external-label ion-input',
+    '.internal-label ion-input',
+    '.internal-label ion-input',
+    '.internal-aria-label ion-input',
+  ];
 
-  it('can be written on by string selector', () => {
-    cy.get(selector)
-      .first()
-      .then(($ionInput) => {
-        const element = $ionInput[0];
-        const wantedText = testHelpers.convertToTestString(element.innerText);
-
-        ionInputCypress.write(selector, wantedText).then(() => {
-          const inputElement = element.querySelector('input')?.value;
-
-          expect(inputElement).to.eq(wantedText);
-        });
+  selectors.forEach((selector) => {
+    describe(selector, () => {
+      beforeEach(() => {
+        cy.visit('./ion-input.html');
       });
-  });
 
-  it('can be cleared', () => {
-    cy.get(selector)
-      .first()
-      .then(($ionInput) => {
-        const element = $ionInput[0];
+      it('can be written on by string selector', () => {
+        cy.get(`${selector}.hydrated`)
+          .first()
+          .then(($ionInput) => {
+            const element = $ionInput[0];
+            const wantedText = testHelpers.convertToTestString(
+              element.innerText
+            );
 
-        ionInputCypress
-          .write(selector, 'i am not clear here :)')
-          .then(() => {
-            return ionInputCypress.clear(selector);
-          })
-          .then(() => {
-            const inputElement = element.querySelector('input')?.value;
+            ionInputCypress.write(selector, wantedText).then(() => {
+              const inputElement = element.querySelector('input')?.value;
 
-            expect(inputElement).to.eq('');
+              expect(inputElement).to.eq(wantedText);
+            });
           });
       });
+
+      it('can be cleared', () => {
+        cy.get(`${selector}.hydrated`)
+          .first()
+          .then(($ionInput) => {
+            const element = $ionInput[0];
+
+            ionInputCypress
+              .write(selector, 'i am not clear here :)')
+              .then(() => {
+                return ionInputCypress.clear(selector);
+              })
+              .then(() => {
+                const inputElement = element.querySelector('input')?.value;
+
+                expect(inputElement).to.eq('');
+              });
+          });
+      });
+    });
   });
 });
